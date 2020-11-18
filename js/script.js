6eb444334f9b0305500e2aed10b4d7e5ec430a14
Diego <sturbadiego25@outlook.it>
2020-11-18 16:25:58 +0100

    inserimento proprietà per rimuovere un elemneto dalla lista Vue js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,15 @@ const app = new Vue({
   el: '#root',
   data: {
     list: [],
+    canc: 'fas fa-times',
     message: null
   },
   methods: {
     insertList: function () {
       this.list.push(this.message);
+    },
+    deleteList: function () {
+      this.list.splice(this.message);
     }
   }
 })
